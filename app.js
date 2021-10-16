3e9281255f77e2b3e515dbb6712c20e8d3d22b4c
ti92 <ti923074@gmail.com>
2021-10-16 18:06:36 +0100

    ajustando identação

--- app.js
+++ app.js
@@ -1,190 +1,112 @@
+const tsideAnime = document.querySelector(".tsideAnime");
+const tsideManga = document.querySelector(".tsideManga");
+const buttonSearch = document.querySelector(".button-search");
+const leWindowOverlay = document.querySelector(".leWindow-overlay");
+const leWindowDOM = document.querySelector(".leWindow");
+const leftHeaderBars = document.querySelector(".left-header-bars");
+const closeLeWindow = document.querySelector(".close-leWindow");
+
+//Listeners para mostrar e fechar a janela de categorias
+
+leftHeaderBars.addEventListener("click", (e) => {
+  leWindowOverlay.classList.add("transparentBcg");
+  leWindowDOM.classList.add("showLeWindow");
+});
+
+closeLeWindow.addEventListener("click", (e) => {
+  leWindowOverlay.classList.remove("transparentBcg");
+
+  leWindowDOM.classList.remove("showLeWindow");
+});
+
+/**/
+
+/*inicio do slider*/
+
+let squareImgs = [];
+let slider;
+let actualImg;
+let maxImg;
+let timeChanges;
+let vtime;
+let vloadOverlay;
+
+function preLoad() {
+  let s = 1;
+
+  for (let i = 0; i < 3; i++) {
+    squareImgs[i] = new Image();
+    squareImgs[i].src = "imgs/s" + s + ".jpeg";
+    s++;
+  }
+}
+
+function loadImg(img) {
+  slider.style.backgroundImage = "url('" + squareImgs[img].src + "')";
+}
+
+function startSlider() {
+  preLoad();
+  actualImg = 0;
+  maxImg = squareImgs.length - 1;
+
+  slider = document.getElementById("dvslider");
+  vloadOverlay = document.getElementById("dvLoadOverlay");
+
+  loadImg(actualImg);
+  timeChanges = 0;
+  anima();
+}
+
+function change(dir) {
+  timeChanges = 0;
+
+  actualImg += dir;
+  if (actualImg > maxImg) {
+    actualImg = 0;
+  } else if (actualImg < 0) {
+    actualImg = maxImg;
+  }
+
+  loadImg(actualImg);
+}
+
+function anima() {
+  timeChanges++;
+  if (timeChanges >= 500) {
+    timeChanges = 0;
+    change(1);
+  }
+
+  vtime = timeChanges / 5;
+  vloadOverlay.style.width = vtime + "%";
+  window.requestAnimationFrame(anima);
+}
+
+window.addEventListener("load", startSlider());
+
+/*fim do slider*/
+
+//Buscar os "top Animes" requisitados e manda como parâmetro para a função topAnimeTela
+const getTopAnime = async () => {
+  const temp = await fetch(
+    `https://api.jikan.moe/v3/top/anime/1/bypopularity`
+  ).then((res) => res.json());
+
+  let topAnime = temp.top.slice(0, 5);
+
+  console.log("topAnime", topAnime);
+  topAnimeTela(topAnime);
+};
+
+getTopAnime();
 
-   
-    const tsideAnime = document.querySelector('.tsideAnime')
-    const tsideManga = document.querySelector('.tsideManga')
-    const buttonSearch = document.querySelector('.button-search')
-    const leWindowOverlay = document.querySelector('.leWindow-overlay');
-    const leWindowDOM =  document.querySelector('.leWindow');                
-    const leftHeaderBars = document.querySelector('.left-header-bars')    
-    const closeLeWindow = document.querySelector('.close-leWindow')
-    
-    
+//Ao receber os parâmetros, alocar cada parte do objeto e alocar nos elementos determinados
+function topAnimeTela(topAnime) {
+  let result = "";
 
-
-
-    //Listeners para mostrar e fechar a janela de categorias 
-
-    leftHeaderBars.addEventListener("click", e =>{
-    
-    
-        leWindowOverlay.classList.add('transparentBcg')
-        leWindowDOM.classList.add('showLeWindow')
-    
-    
-    
-    
-    })
-    
-    
-    closeLeWindow.addEventListener("click", e =>{
-    
-    
-        leWindowOverlay.classList.remove('transparentBcg')
-            
-        leWindowDOM.classList.remove('showLeWindow')
-    
-    
-    
-    
-    })
-
-    /**/
-    
-    /*inicio do slider*/
-    
-    
-    let squareImgs = [];
-    let slider;
-    let actualImg;
-    let maxImg;    
-    let timeChanges;
-    let vtime;
-    let vloadOverlay;
-
-
-    
-    function preLoad(){
-
-        let s=1;
-
-        for(let i=0; i<3; i++){
-        squareImgs[i] = new Image();
-        squareImgs[i].src="imgs/s"+s+".jpeg";
-        s++;
-    }
-
-
-    }
-    
-    
-    function loadImg(img){
-
-        slider.style.backgroundImage="url('"+squareImgs[img].src+"')";
-        
-
-
-    }
-
-
-    function startSlider(){
-        preLoad();
-        actualImg=0;
-        maxImg=squareImgs.length-1;
-
-        slider=document.getElementById("dvslider")
-        vloadOverlay=document.getElementById("dvLoadOverlay")
-
-        loadImg(actualImg);
-        timeChanges=0;
-        anima();
-        
-    }
-
-
-
-    function change(dir){
-
-        timeChanges=0;
-
-        actualImg+=dir;
-        if(actualImg > maxImg){
-
-            actualImg=0;
-
-
-        }else if(actualImg < 0){
-
-            actualImg=maxImg;
-
-        }
-
-        loadImg(actualImg);
-
-
-    }
-
-
-
-    function anima(){
-
-        timeChanges++;
-        if(timeChanges >= 500){
-
-            timeChanges=0;
-            change(1);
-
-
-        }
-
-        vtime=timeChanges/5;
-        vloadOverlay.style.width=vtime+"%";
-        window.requestAnimationFrame(anima);
-
-
-
-    }
-
-
-
-    window.addEventListener("load", startSlider())
-
-    /*fim do slider*/
-
-
-    
-
-
-
-
-
-
-
-    
-    //Buscar os "top Animes" requisitados e manda como parâmetro para a função topAnimeTela
-    const getTopAnime = async () =>{
-
-        const temp = await fetch(`https://api.jikan.moe/v3/top/anime/1/bypopularity`)
-        .then(res => res.json());
-
-        let topAnime = temp.top.slice(0, 5);
-
-
-        console.log("topAnime", topAnime)
-        topAnimeTela(topAnime)
-
-
-    }
-
-
-
-
-
-
-
-    getTopAnime();
-
-
-
-
-
-
-    //Ao receber os parâmetros, alocar cada parte do objeto e alocar nos elementos determinados
-    function topAnimeTela(topAnime){
-
-        let result='';
-
-        topAnime.map(anime =>{
-            result+= `            
+  topAnime.map((anime) => {
+    result += `            
             <div class="trending">            
             <img src=${anime.image_url} alt ="Anime Image"/>
             <a href=${anime.url}>${anime.title}</a>
@@ -194,47 +116,31 @@
             <a href=${anime.url}><button>ver mais+</button></a>
                
             </div>            
-            `           
+            `;
+  });
 
-        })
+  tsideAnime.innerHTML = result;
+}
 
-        tsideAnime.innerHTML = result;
-    }
+//Buscar os "top Mangás" e mandar como parâmetro para a função topMangaTela
+const getTopManga = async () => {
+  const temp1 = await fetch(
+    `https://api.jikan.moe/v3/top/manga/1/bypopularity`
+  ).then((res) => res.json());
 
+  let topManga = temp1.top.slice(0, 5);
 
+  topMangaTela(topManga);
+};
 
+getTopManga();
 
+//Ao receber os parâmetros, alocar cada parte do objeto e alocar nos elementos determinados
+function topMangaTela(topManga) {
+  let result = "";
 
-
-
-
-    //Buscar os "top Mangás" e mandar como parâmetro para a função topMangaTela
-        const getTopManga = async () =>{
-
-        const temp1 = await fetch(`https://api.jikan.moe/v3/top/manga/1/bypopularity`) 
-        .then(res => res.json());
-
-        let topManga = temp1.top.slice(0, 5);
-
-        topMangaTela(topManga);
-
-
-    }
-
-
-    getTopManga();
-
-
-
-    
-    //Ao receber os parâmetros, alocar cada parte do objeto e alocar nos elementos determinados
-    function topMangaTela(topManga){
-
-
-        let result='';
-
-        topManga.map(manga =>{
-            result+= `
+  topManga.map((manga) => {
+    result += `
             
             <div class="trending">            
             <img src=${manga.image_url} alt ="Manga Image"/>
@@ -246,13 +152,8 @@
                
             </div>
 
-            `
+            `;
+  });
 
-
-        })
-
-        tsideManga.innerHTML = result;
-
-
-    }
-
+  tsideManga.innerHTML = result;
+}
--- pesquisa.js
+++ pesquisa.js
@@ -1,57 +1,31 @@
-const resultSearchE = document.querySelector('.result-searchE')
-const leWindowOverlay = document.querySelector('.leWindow-overlay');
-const leWindowDOM =  document.querySelector('.leWindow');
-const closeLeWindow = document.querySelector('.close-leWindow')
-const leftHeaderBars = document.querySelector('.left-header-bars')
-    
-   
-    
-    leftHeaderBars.addEventListener("click", e =>{
-    
-    
-        leWindowOverlay.classList.add('transparentBcg')
-        leWindowDOM.classList.add('showLeWindow')
-    
-    
-    
-    
-    })
+const resultSearchE = document.querySelector(".result-searchE");
+const leWindowOverlay = document.querySelector(".leWindow-overlay");
+const leWindowDOM = document.querySelector(".leWindow");
+const closeLeWindow = document.querySelector(".close-leWindow");
+const leftHeaderBars = document.querySelector(".left-header-bars");
 
+leftHeaderBars.addEventListener("click", (e) => {
+  leWindowOverlay.classList.add("transparentBcg");
+  leWindowDOM.classList.add("showLeWindow");
+});
 
+closeLeWindow.addEventListener("click", (e) => {
+  leWindowOverlay.classList.remove("transparentBcg");
 
+  leWindowDOM.classList.remove("showLeWindow");
+});
 
-    closeLeWindow.addEventListener("click", e =>{
+const fetchAnime = async (query) => {
+  const temp = await fetch(
+    `https://api.jikan.moe/v3/search/anime?q=${query}&order_by=title&sort=asc&limit=15`
+  ).then((res) => res.json());
 
+  let searchedAnimes = temp.results;
 
-    leWindowOverlay.classList.remove('transparentBcg')
-        
-    leWindowDOM.classList.remove('showLeWindow')
+  let result = "";
 
-    })
-
-
-
-
-
-
-
-
-
-    
-    const fetchAnime = async (query) =>{
-
-    const temp = await fetch(`https://api.jikan.moe/v3/search/anime?q=${query}&order_by=title&sort=asc&limit=15`)
-    .then(res => res.json());
-
-
-
-    let searchedAnimes = temp.results;
-
-
-     let result='';
-
-     searchedAnimes.map(anime =>{
-         result+= `
+  searchedAnimes.map((anime) => {
+    result += `
          
          <div class="anime-item">
         
@@ -68,41 +42,23 @@ const leftHeaderBars = document.querySelector('.left-header-bars')
 
          </div>
          
-         `         
+         `;
+  });
 
-     })
-
-     resultSearchE.innerHTML = result;
-
-
-}
-
-
-
-
-
-
-
+  resultSearchE.innerHTML = result;
+};
 
 let GET = {};
 let query = window.location.search.substring(1).split("&");
 
+for (let i = 0, max = query.length; i < max; i++) {
+  if (query[i] === "") continue;
 
-for (let i = 0, max = query.length; i < max; i++){
-  
-    if (query[i] === "")
-        continue;
+  let param = query[i].split("=");
 
-    let param = query[i].split("=");
+  let searchs = param[1].split("+").join(" ");
 
-    let searchs = param[1].split("+").join(" ")
+  GET[decodeURIComponent(searchs)] = decodeURIComponent(searchs || "");
 
-   GET[decodeURIComponent(searchs)] = decodeURIComponent(searchs || "");
-
-
-   fetchAnime(searchs); 
-
-   
+  fetchAnime(searchs);
 }
-
-
